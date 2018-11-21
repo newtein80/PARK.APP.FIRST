8d8fd1637914650d24c60df5ffd92901bb43e783
ParkJongSoo <newtein@naver.com>
2018-11-21 13:15:31 +0900

    2018.11.21.ServerPaging Commit

--- PARK.APP.FIRST/Areas/SystemManage/Controllers/MenuMastersController.cs
+++ PARK.APP.FIRST/Areas/SystemManage/Controllers/MenuMastersController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using PARK.APP.FIRST.Areas.SystemManage.Models.Menu;
 using PARK.APP.FIRST.Data;
 
@@ -24,6 +26,199 @@ namespace PARK.APP.FIRST.Areas.SystemManage.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.MenuMaster.ToListAsync());
+        }
+
+        public class JSONData
+        {
+            public List<MenuMaster> MenuMasters
+            {
+                get;
+                set;
+            }
+            public int TotalRecords
+            {
+                get;
+                set;
+            }
+        }
+
+        [HttpPost]
+        public string GetPageData(string jsonData)
+        {
+            JToken token = JObject.Parse(jsonData);
+
+            List<JToken> filterGroups = token.SelectToken("filterGroups").Children().ToList();
+            int pageSize = (int)token.SelectToken("pagesize");
+            int pageNum = (int)token.SelectToken("pagenum");
+            string sortField = (string)token.SelectToken("sortdataField");
+            string sortOrder = (string)token.SelectToken("sortorder");
+            int count = 0;
+            List<MenuMaster> employees = new List<MenuMaster>();
+            List<MenuMaster> allEmployees = _context.MenuMaster.ToList();
+            if (sortField != "")
+            {
+                if (sortOrder == "asc")
+                {              
+                    allEmployees = (from p in allEmployees
+                                    orderby (p.GetType().GetProperty(sortField).GetValue(p))
+                                 select p).ToList();
+
+                }
+                else if (sortOrder == "desc")
+                {
+                    allEmployees = (from p in allEmployees
+                                    orderby (p.GetType().GetProperty(sortField).GetValue(p)) descending
+                                    select p).ToList();
+                }
+            }
+            if (filterGroups.Count > 0)
+            {
+                List<MenuMaster> filteredEmployees = allEmployees;
+                for (int j = 0; j < filterGroups.Count; j++)
+                {
+                    List<JToken> filters = filterGroups[j].SelectToken("filters").Children().ToList();
+
+                    List<MenuMaster> filterGroup = filteredEmployees;
+                    List<MenuMaster> filterGroupResult = new List<MenuMaster>();
+                    for (int i = 0; i < filters.Count; i++)
+                    {
+                        string filterLabel = (string)filters[i].SelectToken("label");
+                        string filterValue = (string)filters[i].SelectToken("value");
+                        string filterDataField = (string)filters[i].SelectToken("field");
+                        string filterCondition = (string)filters[i].SelectToken("condition");
+                        string filterType = (string)filters[i].SelectToken("type");
+                        string filterOperator = (string)filters[i].SelectToken("operator");
+
+                        List<MenuMaster> currentResult = new List<MenuMaster>();
+
+                        switch (filterCondition)
+                        {
+                            case "NOT_EMPTY":
+                                currentResult = (from p in filterGroup
+                                                 where ((p.GetType().GetProperty(filterDataField).GetValue(p)) != null)
+                                                     select p).ToList();
+                                break;
+                            case "NOT_NULL":
+                                currentResult = (from p in filterGroup
+                                                 where ((p.GetType().GetProperty(filterDataField).GetValue(p)).ToString() != "")
+                                                     select p).ToList();
+                                break;
+                            case "NULL":
+                                currentResult = (from p in filterGroup
+                                                 where ((p.GetType().GetProperty(filterDataField).GetValue(p)) == null)
+                                                     select p).ToList();
+                                break;
+                            case "EMPTY":
+                                currentResult = (from p in filterGroup
+                                                 where ((p.GetType().GetProperty(filterDataField).GetValue(p)).ToString() == "")
+                                                     select p).ToList();
+                                break;
+                            case "CONTAINS_CASE_SENSITIVE":
+                                currentResult = (from p in filterGroup
+                                                 where ((p.GetType().GetProperty(filterDataField).GetValue(p)).ToString().Contains(filterValue))
+                                                     select p).ToList();
+                                break;
+                            case "CONTAINS":
+                                currentResult = (from p in filterGroup
+                                                 where ((p.GetType().GetProperty(filterDataField).GetValue(p)).ToString().IndexOf(filterValue, StringComparison.CurrentCultureIgnoreCase) != -1)
+                                                     select p).ToList();
+                                break;
+                            case "DOES_NOT_CONTAIN_CASE_SENSITIVE":
+                                currentResult = (from p in filterGroup
+                                                 where (!(p.GetType().GetProperty(filterDataField).GetValue(p)).ToString().Contains(filterValue))
+                                                     select p).ToList();
+                                break;
+                            case "DOES_NOT_CONTAIN":
+                                currentResult = (from p in filterGroup
+                                                 where ((p.GetType().GetProperty(filterDataField).GetValue(p)).ToString().IndexOf(filterValue, StringComparison.CurrentCultureIgnoreCase) == -1)
+                                                     select p).ToList();
+                                break;
+                            case "EQUAL_CASE_SENSITIVE":
+                                currentResult = (from p in filterGroup
+                                                 where ((p.GetType().GetProperty(filterDataField).GetValue(p)).ToString() == filterValue)
+                                                     select p).ToList();
+                                break;
+                            case "EQUAL":
+                                currentResult = (from p in filterGroup
+                                                 where ((p.GetType().GetProperty(filterDataField).GetValue(p)).ToString().IndexOf(filterValue, StringComparison.CurrentCultureIgnoreCase) == 0)
+                                                     select p).ToList();
+                                break;
+                            case "NOT_EQUAL_CASE_SENSITIVE":
+                                currentResult = (from p in filterGroup
+                                                 where ((p.GetType().GetProperty(filterDataField).GetValue(p)).ToString() != filterValue)
+                                                     select p).ToList();
+                                break;
+                            case "NOT_EQUAL":
+                                currentResult = (from p in filterGroup
+                                                 where ((p.GetType().GetProperty(filterDataField).GetValue(p)).ToString().IndexOf(filterValue, StringComparison.CurrentCultureIgnoreCase) != 0)
+                                                     select p).ToList();
+                                break;
+                            case "GREATER_THAN":
+                                currentResult = (from p in filterGroup
+                                                 where (float.Parse(p.GetType().GetProperty(filterDataField).GetValue(p).ToString()) > float.Parse(filterValue))
+                                                     select p).ToList();
+                                break;
+                            case "LESS_THAN":
+                                currentResult = (from p in filterGroup
+                                                 where (float.Parse(p.GetType().GetProperty(filterDataField).GetValue(p).ToString()) < float.Parse(filterValue))
+                                                     select p).ToList();
+                                break;
+                            case "GREATER_THAN_OR_EQUAL":
+                                currentResult = (from p in filterGroup
+                                                 where (float.Parse(p.GetType().GetProperty(filterDataField).GetValue(p).ToString()) >= float.Parse(filterValue))
+                                                     select p).ToList();
+                                break;
+                            case "LESS_THAN_OR_EQUAL":
+                                currentResult = (from p in filterGroup
+                                                 where (float.Parse(p.GetType().GetProperty(filterDataField).GetValue(p).ToString()) <= float.Parse(filterValue))
+                                                     select p).ToList();
+                                break;
+                            case "STARTS_WITH_CASE_SENSITIVE":
+                                currentResult = (from p in filterGroup
+                                                 where ((p.GetType().GetProperty(filterDataField).GetValue(p)).ToString().StartsWith(filterValue))
+                                                     select p).ToList();
+                                break;
+                            case "STARTS_WITH":
+                                currentResult = (from p in filterGroup
+                                                 where ((p.GetType().GetProperty(filterDataField).GetValue(p)).ToString().StartsWith(filterValue, StringComparison.CurrentCultureIgnoreCase))
+                                                     select p).ToList();
+                                break;
+                            case "ENDS_WITH_CASE_SENSITIVE":
+                                currentResult = (from p in filterGroup
+                                                 where ((p.GetType().GetProperty(filterDataField).GetValue(p)).ToString().EndsWith(filterValue))
+                                                     select p).ToList();
+                                break;
+                            case "ENDS_WITH":
+                                currentResult = (from p in filterGroup
+                                                 where ((p.GetType().GetProperty(filterDataField).GetValue(p)).ToString().EndsWith(filterValue, StringComparison.CurrentCultureIgnoreCase))
+                                                     select p).ToList();
+                                break;
+                        }
+
+                        if (filterOperator == "or")
+                        {
+                            filterGroupResult.AddRange(currentResult);
+                        }
+                        else
+                        {
+                            filterGroup = currentResult;
+                            filterGroupResult = currentResult;
+                        }
+                    }
+                    filteredEmployees = filterGroupResult;
+                }
+                allEmployees = filteredEmployees;
+             }
+                 
+            for (int i = pageNum * pageSize; i < allEmployees.Count && count < pageSize; i++)
+            {
+                employees.Add(allEmployees[i]);
+                count++;
+            }
+            JSONData data = new JSONData();
+            data.TotalRecords = allEmployees.Count;
+            data.MenuMasters = employees;
+            return JsonConvert.SerializeObject(data);
         }
 
         // GET: SystemManage/MenuMasters/Details/5
